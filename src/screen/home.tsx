5fb469cf18cb251eb74246cbcefd3f42c5279c53
Giang Hoang <gianghoang.9955@gmail.com>
2021-05-23 16:37:45 +0700

    handle choice option dialog

--- src/screen/home.tsx
+++ src/screen/home.tsx
@@ -1,20 +1,25 @@
 // import React from 'react'
-// import DialogComponent from '../common/dialog'
+import DialogComponent from '../common/dialog'
 
 // import Constant from "../const.json";
 // import axios from "axios";
 
 function HomePage(props: any) {
+  function handleDialogChoice(choice: boolean) {
+    console.log(choice)
+  }
+
   return (
     <div>
       {/* test dialog confirm */}
-      {/* <DialogComponent
+      <DialogComponent
         title="Confirm Dialog"
         message="Bạn Quý có ăn cứt hong ?"
         buttonAccept="Ok"
-        buttonCancel="No"
+        buttonCancel="yes"
         isValid={false}
-      /> */}
+        handleDialogChoice={handleDialogChoice}
+      />
       Home
     </div>
   )
