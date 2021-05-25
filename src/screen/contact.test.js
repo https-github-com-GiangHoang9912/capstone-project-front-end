263c2c8180bd071d2592b0d8eda302e3b2cddc66
tutatj84 <40205190+tutatj84@users.noreply.github.com>
2021-05-25 21:06:13 +0700

    Delete contact.test.js

--- src/screen/contact.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import HomePage from './home';
-// import Login 
-test('renders learn react link', () => {
-  render(<HomePage />);
-  const linkElement = screen.getByText(/Home Page/i);
-  expect(linkElement).toBeInTheDocument();
-});
