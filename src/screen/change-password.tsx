b604a37b94872bb6bb013da82626db7ad069ed6c
Giang Hoang <gianghoang.9955@gmail.com>
2021-06-13 19:32:23 +0700

    format file

--- src/screen/change-password.tsx
+++ src/screen/change-password.tsx
@@ -1,29 +1,25 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable max-len */
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useState, useCallback } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useCallback } from 'react'
+import PropTypes from 'prop-types'
 
-import {
-  Link
-} from "react-router-dom";
+import { Link } from 'react-router-dom'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheck, faLock, faEye } from '@fortawesome/free-solid-svg-icons'
-import styled from 'styled-components';
-
+import styled from 'styled-components'
 
 ChangePassword.propTypes = {
   className: PropTypes.string,
 }
 
 ChangePassword.defaultProps = {
-  className: ''
+  className: '',
 }
 
-
 function ChangePassword(props: any) {
-  const { className } = props;
+  const { className } = props
 
   const [iconList, setIconList] = useState([
     {
@@ -37,16 +33,18 @@ function ChangePassword(props: any) {
     {
       id: 3,
       showPassword: false,
-    }
-  ]);
+    },
+  ])
 
   const onCheckBtnClick = useCallback((id) => {
     setIconList((prevState) =>
       prevState.map((icon) =>
-        icon.id === id ? { ...icon, showPassword: !icon.showPassword } : { ...icon, showPassword: icon.showPassword }
+        icon.id === id
+          ? { ...icon, showPassword: !icon.showPassword }
+          : { ...icon, showPassword: icon.showPassword }
       )
-    );
-  }, []);
+    )
+  }, [])
 
   return (
     <div className={className}>
@@ -59,66 +57,72 @@ function ChangePassword(props: any) {
               </div>
               <div className="detail-rules">
                 <h5>Password must contains: </h5>
-                <p><FontAwesomeIcon className="checked" icon={faCheck} />At least 6 characters.</p>
-                <p><FontAwesomeIcon className="checked" icon={faCheck} />At least 1 upper case letter (A-Z).</p>
-                <p><FontAwesomeIcon className="checked" icon={faCheck} />At least 1 lower case letter (a-z).</p>
-                <p><FontAwesomeIcon className="checked" icon={faCheck} />At least 1 number (0-9).</p>
+                <p>
+                  <FontAwesomeIcon className="checked" icon={faCheck} />
+                  At least 6 characters.
+                </p>
+                <p>
+                  <FontAwesomeIcon className="checked" icon={faCheck} />
+                  At least 1 upper case letter (A-Z).
+                </p>
+                <p>
+                  <FontAwesomeIcon className="checked" icon={faCheck} />
+                  At least 1 lower case letter (a-z).
+                </p>
+                <p>
+                  <FontAwesomeIcon className="checked" icon={faCheck} />
+                  At least 1 number (0-9).
+                </p>
               </div>
             </div>
             <form className="login-area-form">
               <div className="password">
-                <input 
-                  type={iconList[0].showPassword ? "text" : "password"}
+                <input
+                  type={iconList[0].showPassword ? 'text' : 'password'}
                   key={iconList[0].id}
                   className="input-pass"
                   placeholder="Enter old password"
-                  required />
+                  required
+                />
                 <span className="icon-pass">
                   <FontAwesomeIcon icon={faLock} />
                 </span>
                 <span className="icon-eye">
-                  <FontAwesomeIcon
-                    icon={faEye}
-                    onClick={() => onCheckBtnClick(iconList[0].id)}
-                  />
+                  <FontAwesomeIcon icon={faEye} onClick={() => onCheckBtnClick(iconList[0].id)} />
                 </span>
               </div>
               <div className="password">
-                <input 
-                  type={iconList[1].showPassword ? "text" : "password"}
+                <input
+                  type={iconList[1].showPassword ? 'text' : 'password'}
                   key={iconList[1].id}
                   className="input-pass"
-                  placeholder="Enter new password" required />
+                  placeholder="Enter new password"
+                  required
+                />
                 <span className="icon-pass">
                   <FontAwesomeIcon icon={faLock} />
                 </span>
                 <span className="icon-eye">
-                  <FontAwesomeIcon
-                    icon={faEye}
-                    onClick={() => onCheckBtnClick(iconList[1].id)}
-                  />
+                  <FontAwesomeIcon icon={faEye} onClick={() => onCheckBtnClick(iconList[1].id)} />
                 </span>
               </div>
               <div className="password">
-                <input 
-                  type={iconList[2].showPassword ? "text" : "password"}
+                <input
+                  type={iconList[2].showPassword ? 'text' : 'password'}
                   key={iconList[2].id}
                   className="input-pass"
-                  placeholder="Re_Enter new password" required />
+                  placeholder="Re_Enter new password"
+                  required
+                />
                 <span className="icon-pass">
                   <FontAwesomeIcon icon={faLock} />
                 </span>
                 <span className="icon-eye">
-                  <FontAwesomeIcon
-                    icon={faEye}
-                    onClick={() => onCheckBtnClick(iconList[2].id)}
-                  />
+                  <FontAwesomeIcon icon={faEye} onClick={() => onCheckBtnClick(iconList[2].id)} />
                 </span>
               </div>
               <div className="contain-btn">
-                <button className="btn-login">
-                  Login
-                </button>
+                <button className="btn-login">Login</button>
               </div>
               <div className="text-process">
                 <a className="cancel" href="#">
@@ -130,240 +134,240 @@ function ChangePassword(props: any) {
         </div>
       </div>
     </div>
-  );
+  )
 }
 
 const StyledForgotPassword = styled(ChangePassword)`
-body,
-html {
-  height: 100%;
-  font-family: sans-serif;
-}
-
-.limiter {
-  width: 100%;
-  margin: 0 auto;
-}
-
-.container {
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-items: center;
-  padding: 15px;
-  background: #f5f6fa;
-}
-.wrap-login {
-  width: 960px;
-  background: #fff;
-  overflow: hidden;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  padding: 75px 130px 102px 95px;
-  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-}
-
-/*  css for detail rules*/
-.rules-account {
-  width: 316px;
-}
-.rules-account {
-  max-width: 100%;
-}
-.title h2 {
-  font-family: Arial, Helvetica, sans-serif;
-}
-.detail-rules {
-  padding-top: 20px;
-}
-.detail-rules h5 {
-  margin-bottom: 5px;
-  font-size: 17px;
-}
-.detail-rules p {
-  font-size: 15px;
-  padding: 10px;
-  font-family: Georgia, "Times New Roman", Times, serif;
-}
-.detail-rules .checked {
-  color: #2980b9;
-}
-/* Input css */
-.input-pass {
-  outline: none;
-  border: none;
-}
-.input-pass:focus {
-  animation: pulse-animation 1.5s infinite;
-}
-
-@keyframes pulse-animation {
-  0% {
-    box-shadow: 0 0 0 0px rgba(32, 182, 45, 0.527);
+  body,
+  html {
+    height: 100%;
+    font-family: sans-serif;
   }
-  100% {
-    box-shadow: 0 0 0 20px rgba(0, 0, 0, 0);
+
+  .limiter {
+    width: 100%;
+    margin: 0 auto;
   }
-}
-.password {
-  position: relative;
-  width: 100%;
-  z-index: auto;
-  margin-bottom: 10px;
-}
-.input-pass {
-  font-family: sans-serif;
-  font-size: 15px;
-  line-height: 1.5;
-  color: #666666;
-  display: block;
-  width: 100%;
-  background: #ecf0f1;
-  height: 50px;padding: 0 30px 0 68px;
-  
-  cursor: pointer;
-}
-.icon-pass {
-  display: flex;
-  align-items: center;
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  height: 100%;
-  padding-left: 25px;
-}
-.icon-eye {
-  display: flex;
-  align-items: center;
-  position: absolute;
-  bottom: 0;
-  right: 0;
-  font-size: 15px;
-  height: 100%;
-  padding-right: 20px;
-}
 
-/* button login */
-
-.contain-btn {
-  position: relative;
-  width: 100%;
-  z-index: auto;
-  margin-bottom: 10px;
-  margin-top: 25px;
-}
-.btn-login {
-  font-family: Helvetica;
-  font-size: 23px;
-  font-weight: bold;
-  line-height: 1.5;
-  position: absolute;
-  color: #fff;
-  display: block;
-  width: 100%;
-  background: #1e90ff;
-  height: 50px;
-  padding: 0 30px 0 68px;
-  cursor: pointer;
-  align-items: center;
-  padding-left: 42px;
-}
-.btn-login {
-  outline: none;
-  border: none;
-}
-.btn-login:hover {
-  background-color: #273c75;
-}
-
-/* text css */
-
-.text-process {
-  margin-top: 85px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-}
-
-.cancel {
-  font-family: Poppins-Regular;
-  font-size: 22px;
-  line-height: 1.5;
-  color: #666666;
-  text-decoration: none;
-}
-.cancel:hover {
-  cursor: pointer;
-  color:  tomato;
-}
-
-/* Responsive */
-@media (max-width: 992px) {
+  .container {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    padding: 15px;
+    background: #f5f6fa;
+  }
   .wrap-login {
-    padding: 177px 90px 33px 85px;
+    width: 960px;
+    background: #fff;
+    overflow: hidden;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    padding: 75px 130px 102px 95px;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
   }
 
+  /*  css for detail rules*/
   .rules-account {
-    width: 35%;
+    width: 316px;
+  }
+  .rules-account {
+    max-width: 100%;
+  }
+  .title h2 {
+    font-family: Arial, Helvetica, sans-serif;
+  }
+  .detail-rules {
+    padding-top: 20px;
+  }
+  .detail-rules h5 {
+    margin-bottom: 5px;
+    font-size: 17px;
+  }
+  .detail-rules p {
+    font-size: 15px;
+    padding: 10px;
+    font-family: Georgia, 'Times New Roman', Times, serif;
+  }
+  .detail-rules .checked {
+    color: #2980b9;
+  }
+  /* Input css */
+  .input-pass {
+    outline: none;
+    border: none;
+  }
+  .input-pass:focus {
+    animation: pulse-animation 1.5s infinite;
   }
 
-  .login-area-form {
-    width: 50%;
+  @keyframes pulse-animation {
+    0% {
+      box-shadow: 0 0 0 0px rgba(32, 182, 45, 0.527);
+    }
+    100% {
+      box-shadow: 0 0 0 20px rgba(0, 0, 0, 0);
+    }
   }
-}
-
-@media (max-width: 768px) {
-  .wrap-login {
-    padding: 100px 80px 33px 80px;
-    display: flex;
-    flex-direction: column;
-    text-align: center;
-
-  }
-  .login-area-form {
+  .password {
+    position: relative;
     width: 100%;
+    z-index: auto;
+    margin-bottom: 10px;
   }
-  .detail-rules h5, p{
-    display: none;
-  }
-  .title-h2{
-    display:inline-block;
-    font-size: 20px;
-    width: 200px;
-    margin-left: 80px;
-  }
-}
-
-@media (max-width: 576px) {
-  .wrap-login {
-    padding: 100px 15px 33px 15px;
-  }
-  .wrap-login {
-    padding: 100px 80px 33px 80px;
-    display: flex;
-    flex-direction: column;
-  }
-  .login-area-form {
+  .input-pass {
+    font-family: sans-serif;
+    font-size: 15px;
+    line-height: 1.5;
+    color: #666666;
+    display: block;
     width: 100%;
-  }
-  .detail-rules h5, p{
-    display: none;
-  }
-   .title{
-    text-align: center;
-  }
-   
-  .title-h2{
-    display:inline-block;
-    font-size: 20px;
-    width: 200px;
-    margin-left: 80px;
-  }
-}
+    background: #ecf0f1;
+    height: 50px;
+    padding: 0 30px 0 68px;
 
+    cursor: pointer;
+  }
+  .icon-pass {
+    display: flex;
+    align-items: center;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    height: 100%;
+    padding-left: 25px;
+  }
+  .icon-eye {
+    display: flex;
+    align-items: center;
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    font-size: 15px;
+    height: 100%;
+    padding-right: 20px;
+  }
+
+  /* button login */
+
+  .contain-btn {
+    position: relative;
+    width: 100%;
+    z-index: auto;
+    margin-bottom: 10px;
+    margin-top: 25px;
+  }
+  .btn-login {
+    font-family: Helvetica;
+    font-size: 23px;
+    font-weight: bold;
+    line-height: 1.5;
+    position: absolute;
+    color: #fff;
+    display: block;
+    width: 100%;
+    background: #1e90ff;
+    height: 50px;
+    padding: 0 30px 0 68px;
+    cursor: pointer;
+    align-items: center;
+    padding-left: 42px;
+  }
+  .btn-login {
+    outline: none;
+    border: none;
+  }
+  .btn-login:hover {
+    background-color: #273c75;
+  }
+
+  /* text css */
+
+  .text-process {
+    margin-top: 85px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .cancel {
+    font-family: Poppins-Regular;
+    font-size: 22px;
+    line-height: 1.5;
+    color: #666666;
+    text-decoration: none;
+  }
+  .cancel:hover {
+    cursor: pointer;
+    color: tomato;
+  }
+
+  /* Responsive */
+  @media (max-width: 992px) {
+    .wrap-login {
+      padding: 177px 90px 33px 85px;
+    }
+
+    .rules-account {
+      width: 35%;
+    }
+
+    .login-area-form {
+      width: 50%;
+    }
+  }
+
+  @media (max-width: 768px) {
+    .wrap-login {
+      padding: 100px 80px 33px 80px;
+      display: flex;
+      flex-direction: column;
+      text-align: center;
+    }
+    .login-area-form {
+      width: 100%;
+    }
+    .detail-rules h5,
+    p {
+      display: none;
+    }
+    .title-h2 {
+      display: inline-block;
+      font-size: 20px;
+      width: 200px;
+      margin-left: 80px;
+    }
+  }
+
+  @media (max-width: 576px) {
+    .wrap-login {
+      padding: 100px 15px 33px 15px;
+    }
+    .wrap-login {
+      padding: 100px 80px 33px 80px;
+      display: flex;
+      flex-direction: column;
+    }
+    .login-area-form {
+      width: 100%;
+    }
+    .detail-rules h5,
+    p {
+      display: none;
+    }
+    .title {
+      text-align: center;
+    }
+
+    .title-h2 {
+      display: inline-block;
+      font-size: 20px;
+      width: 200px;
+      margin-left: 80px;
+    }
+  }
 `
 export default StyledForgotPassword
