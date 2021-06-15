0373b4754c6d8c7d87b9f1126be2a3c1e07658e4
Giang Hoang <giangnhhe130936@fpt.edu.vn>
2021-06-15 13:31:29 +0000

    update call api backend

--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://localhost:3001'
+export const BASE_URL = "https://ddsgq.xyz/api"
 export const GOOGLE_CLIENT_ID =
   '827399353225-6da9iooquukb62dosd6sdbddp6jo0k8a.apps.googleusercontent.com'
