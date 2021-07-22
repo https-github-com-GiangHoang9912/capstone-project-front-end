dea6d66abc12dedbf447577b26b8b527baaf82ef
Giang Hoang <gianghoang.9955@gmail.com>
2021-07-22 17:16:43 +0700

    update create exam

--- src/screen/list-exam.tsx
+++ src/screen/list-exam.tsx
@@ -56,6 +56,7 @@ interface Question {
 const GET_SUBJECT_URL = `${CONSTANT.BASE_URL}/subject`;
 const GET_EXAM_URL = `${CONSTANT.BASE_URL}/exam`;
 const GET_QUESTION_URL1 = `${CONSTANT.BASE_URL}/question`;
+const CREATE_EXAM_URL = `${CONSTANT.BASE_URL}/exam/create-exam`;
 ListExam.propTypes = {
   className: PropTypes.string,
 };
@@ -139,6 +140,8 @@ function ListExam(props: any) {
   ]);
   const [subject, setSubject] = useState<Subject[]>([
   ]);
+
+  const [subjectId, setSubjectId] = useState<Number>(1);
 
   const [question, setQuestion] = useState<Question[]>([
   ]);
@@ -240,6 +243,7 @@ function ListExam(props: any) {
   /* Event when click button create exam */
   const handleChange = (event: any) => {
     // setSubject((event.target.value) || '');
+    setSubjectId(Number(event.target.value))
   };
   const handleClickBtnCreate = () => {
     setOpenDialogCreate(true);
@@ -315,7 +319,7 @@ function ListExam(props: any) {
                 <InputLabel htmlFor="demo-dialog-native">Subject</InputLabel>
                 <Select
                   native
-                  value={subject}
+                  value={subjectId}
                   onChange={handleChange}
                   input={<Input id="demo-dialog-native" />}
                 >
@@ -352,6 +356,20 @@ function ListExam(props: any) {
       </div>
     </div >
   );
+
+  const handleCreateExam = async (e: any) => {
+    e.preventDefault()
+    console.log(subjectId)
+    console.log(txtNameExam)
+    const response = await axios.post(`${CREATE_EXAM_URL}/${idUser}`, {
+      subjectId,
+      examName: txtNameExam
+    })
+
+    if (response) {
+      console.log(response)
+    }
+  }
 
   return (
     <div className={className}>
@@ -395,7 +413,7 @@ function ListExam(props: any) {
                   <Button onClick={handleCloseCreate} color="primary">
                     Cancel
                   </Button>
-                  <Button color="primary">
+                  <Button color="primary" onClick={handleCreateExam}>
                     Create
                   </Button>
                 </DialogActions>
