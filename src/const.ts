b21354ac49eda4541cbb71202cf553c339adc7f9
tienna <tiennahe130396@fpt.edu.vn>
2021-08-18 18:12:43 +0700

    change base url

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3001/api'
+export const BASE_URL = 'https://ddsgq.xyz/api/'
 export const GOOGLE_CLIENT_ID =
   '827399353225-mcijnq4m11fumgle6f14o8jfre24t9e7.apps.googleusercontent.com'
 export const GOOGLE_CLIENT_ID_LOCAL =
